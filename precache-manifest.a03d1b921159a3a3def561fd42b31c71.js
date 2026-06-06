240ce5243b00e7b614863fbbec1fc4cdc23faa48
Tejesh-45 <Tejesh-45@users.noreply.github.com>
2026-06-06 05:39:16 +0000

    deploy: 1a52005a7c1ed7ae2bee5c6fda36659c289c1c54

---
+++ precache-manifest.a03d1b921159a3a3def561fd42b31c71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf3ab1027a89c6d1baf92422f08225b",
+    "url": "/aqi-analysis/index.html"
+  },
+  {
+    "revision": "39d4a227e7d6731eb88c",
+    "url": "/aqi-analysis/static/css/main.78d5190a.chunk.css"
+  },
+  {
+    "revision": "e8eaa2c571761fcd116e",
+    "url": "/aqi-analysis/static/js/2.d7fb9814.chunk.js"
+  },
+  {
+    "revision": "39d4a227e7d6731eb88c",
+    "url": "/aqi-analysis/static/js/main.58657cae.chunk.js"
+  },
+  {
+    "revision": "16811123e26d1caaa1e0",
+    "url": "/aqi-analysis/static/js/runtime-main.61065e4a.js"
+  },
+  {
+    "revision": "17610ce79f101daee426c7c961224dae",
+    "url": "/aqi-analysis/static/media/search-icon.17610ce7.bin"
+  }
+]);
